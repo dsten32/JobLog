d0889c52cac3a72eee880dadfa78c70e2b45192c
Darcy Cowan <dsten32@gmail.com>
2019-05-17 21:17:52 +1200

    got buttons to disappear on landscape orientation.

--- app/src/main/java/com/comp575/myapplication/MainActivity.java
+++ app/src/main/java/com/comp575/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.comp575.myapplication;
 
+import android.content.Context;
 import android.os.Parcelable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,7 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 
 import android.os.Bundle;
+import android.view.Surface;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.SearchView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -30,37 +33,43 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     //search variables
     private SearchView editSearch;
 
+    private Context context;
+
+    private int screenOrientation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = getApplicationContext();
+        screenOrientation = ((WindowManager)  context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getOrientation();
         setContentView(R.layout.activity_main);
+        if(screenOrientation==3){
+            View listbutton = findViewById(R.id.listbutton);
+            listbutton.setVisibility(View.GONE);
+            View contactbutton = findViewById(R.id.editbutton);
+            contactbutton.setVisibility(View.GONE);
+        }
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, contacts);
         //adapter setup
-        if(findViewById(R.id.contactsListView)!=null) {
+        if (findViewById(R.id.contactsListView) != null) {
             contactsListView = findViewById(R.id.contactsListView);
             contactsListView.setAdapter(adapter);
             contactsListView.setOnItemClickListener(this);
-
         }
-
 
 
         // Locate the EditText in listview_main.xml, or not id portrait view
         editSearch = findViewById(R.id.search);
-        if(editSearch != null) {
+        if (editSearch != null) {
             editSearch.setOnQueryTextListener(this);
         }
 
-        if(savedInstanceState != null){
-            for (Parcelable contact : savedInstanceState.getParcelableArrayList("contacts")){
+        if (savedInstanceState != null) {
+            for (Parcelable contact : savedInstanceState.getParcelableArrayList("contacts")) {
                 contacts.add((Contact) contact);
             }
-        } /*else {
-            //hardwired contacts for testing
-            contacts.add(new Contact("random Name", "email@place.com", "09856466"));
-            contacts.add(new Contact("cooler Name", "email@place.com", "09856466"));
-        }*/
+        }
 
         contactRepository = new ContactRepository(this);
         contactRepository.getAllContacts().observe(this, new Observer<List<Contact>>() {
@@ -74,7 +83,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     }
 
-    public void saveContact(View view){
+    public void saveContact(View view) {
         String status;
         EditText nameField = findViewById(R.id.name);
         String name = nameField.getText().toString();
@@ -82,36 +91,36 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         String email = emailField.getText().toString();
         EditText phoneField = findViewById(R.id.phone);
         String phone = phoneField.getText().toString();
-        Contact contact=new Contact(name,email,phone);
+        Contact contact = new Contact(name, email, phone);
 
         //see if contact is in the ArrayList
-        if (contacts.contains(contact)){
+        if (contacts.contains(contact)) {
             Contact existingContact = contacts.get(contacts.indexOf(contact));
             contact.id = existingContact.id;
             contactRepository.update(contact);
-            status="Updated";
+            status = "Updated";
 //            contacts.set(contacts.indexOf(contact),contact);
         } else {
             contactRepository.insert(contact);
-            status="Saved";
+            status = "Saved";
 //            contacts.add(contact);
         }
 
         adapter.notifyDataSetChanged();
-        Toast.makeText(this,status + " contact details for \n" + contact.name + "\nEmail: " + contact.email + "\nMobile: " + contact.mobile,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, status + " contact details for \n" + contact.name + "\nEmail: " + contact.email + "\nMobile: " + contact.mobile, Toast.LENGTH_SHORT).show();
     }
 
-    public void deleteContact(View view){
+    public void deleteContact(View view) {
         EditText nameField = findViewById(R.id.name);
         String name = nameField.getText().toString();
         EditText emailField = findViewById(R.id.email);
         String email = emailField.getText().toString();
         EditText phoneField = findViewById(R.id.phone);
         String phone = phoneField.getText().toString();
-        Contact contact=new Contact(name,email,phone);
+        Contact contact = new Contact(name, email, phone);
 
         //see if contact is in the ArrayList
-        if (contacts.contains(contact)){
+        if (contacts.contains(contact)) {
             Contact existingContact = contacts.get(contacts.indexOf(contact));
             contact.id = existingContact.id;
             contactRepository.delete(contact);
@@ -122,8 +131,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         }
 
         adapter.notifyDataSetChanged();
-        Toast.makeText(this,"Deleted contact details for \n" + contact.name,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Deleted contact details for \n" + contact.name, Toast.LENGTH_SHORT).show();
     }
+
+
 
     @Override
     protected void onSaveInstanceState(Bundle savedState) {
